552f8012460ad1609e64592632f1d62275a7af4b
Roga <ryanroga@gmail.com>
2023-07-18 11:21:25 -0300

    Working
    Needs improvement, but technically works.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
-import svelte from "@astrojs/svelte";
+import svelte from '@astrojs/svelte'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
-});
+  integrations: [tailwind(), svelte()],
+  output: 'server',
+  env: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    OPENAI_ORG_ID: process.env.OPENAI_ORG_ID,
+  },
+})
